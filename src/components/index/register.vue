3426ed08a56f5e068898ec94e278836301ca73aa
Bulingxi <1648530095@qq.com>
2019-07-25 18:41:14 +0800

    修改了注册页面，正则表达式

--- src/components/index/register.vue
+++ src/components/index/register.vue
@@ -23,8 +23,17 @@
 			<div class="box">
 				<p class="welcome">欢迎注册QQ</p>
 				<p class="everday">每一天，乐在沟通。<span class="free">免费靓号</span></p>
-				<input type="text" class="user" placeholder="昵称"/>
-				<input type="password" class="user pwd" placeholder="密码"/>
+				<input type="text" class="user" placeholder="昵称" v-model="user" @focus="isfocus()"/>
+				<p class="error-tips"><img src="../../assets/images/error.png"/>昵称不可以为空</p>
+				<input type="password" class="user pwd" placeholder="密码" v-model="pwd"  @focus="isfocus2()" @blur="isblur()"/>
+				<p class="error-tips err-pwd"><img src="../../assets/images/error.png"/>密码不可以为空</p>
+				<div class="pwd-onfo">
+					<p class="green"><img src="../../assets/images/green.png"/>不能包括空格</p>
+					<p class="info"><img src="../../assets/images/info.png"/>长度为8-16个字符</p>
+					<p class="info"><img src="../../assets/images/info.png"/>必须包含字母、数字、符号中至少2种</p>
+				</div>
+				<p class="error-tips err-pwd"><img src="../../assets/images/error.png"/>不能有空格</p>
+				<p class="error-tips err-pwd"><img src="../../assets/images/error.png"/>长度为8-16个字符</p>
 				<select id="qz">
 					<option>+86</option>
 					<option>中国 +86</option>
@@ -32,9 +41,11 @@
 					<option>中国澳门特别行政区 +853</option>
 					<option>中国台湾地区 +886</option>
 				</select>
+				
+				<!--<ul class=""></ul>-->
 				<input type="tel" class="user register-tel" placeholder="手机号" />
 				<p class="input-tips">可通过该手机号找回密码</p>
-				<input type="submit" class="submit" value="立即注册"/>
+				<input type="submit" class="submit" value="立即注册" @click="register()"/>
 				<input type="checkbox" class="check" />
 				<span class="agree">我已阅读并同意相关服务条款和隐私政策</span>
 			</div>
@@ -43,18 +54,59 @@
 </template>
 
 <script>
-
+var err=document.getElementsByClassName('error-tips');
+var pwdinfo=document.getElementsByClassName('pwd-onfo');
 	export default{
 		data(){
 			return{
-				lanshow:false
+				lanshow:false,
+				user:'',
+				pwd:''
 			}
 		},
 		methods:{
 			downlist(){
 				
 				this.lanshow=!this.lanshow;
+			},
+			register(){
+				var reg =/\s/;
+				var reg2=/^[A-Za-z0-9]{8,16}$/;
+				if(this.user==''&&this.pwd==''){
+					err[0].style.height="auto"
+					err[1].style.height="auto"
+				}else if(reg.test(this.pwd)){
+					pwdinfo[0].style.height=0
+					err[2].style.height="auto"
+				}else if(!reg.test(this.pwd) && reg2.test(this.pwd) ){
+					pwdinfo[0].style.height=0
+				}
+				else if(reg2.test(this.pwd)){
+					pwdinfo[0].style.height=0
+					
+				}else if(!reg2.test(this.pwd)){
+					pwdinfo[0].style.height=0;
+					err[3].style.height='auto'
+				}
+			},
+			isfocus(){
+				err[0].style.height=0;
+			},
+			isfocus2(){
+				err[1].style.height=0;
+				pwdinfo[0].style.height="auto"
+				err[2].style.height=0;
+				err[3].style.height=0
+			},
+			isblur(){
+				if(this.pwd==''){
+					pwdinfo[0].style.height=0
+				}
+				
 			}
+		},
+		mounted(){
+			
 		}
 	}
 </script>
@@ -186,6 +238,52 @@
 		border: 1px solid #aaa;
 		margin-top: 20px;
 		outline: none;
+	}
+	.error-tips{
+		width: 100%;
+		height: 0;
+		overflow: hidden;
+		display: block;
+		/*padding-top: 10px;*/
+		font-size: 14px;
+		color: #ff5b5b;
+		transition: all 0.65s;
+	}
+	
+	.error-tips img{
+		width: 18px;
+		height: 18px;
+		display: inline-block;
+		vertical-align: middle;
+		padding-right: 10px;
+	}
+	.pwd-onfo{
+		width: 100%;
+		height: 0;
+		overflow: hidden;
+		/*padding-top: 10px;*/
+		transition: all 0.65s;
+	}
+	.green,.info{
+		width: 100%;
+		display: block;
+		overflow: hidden;
+		font-size: 14px;
+		margin-top: 5px;
+	}
+	.green img{
+		width: 18px;
+		height: 18px;
+		vertical-align: middle;
+		padding-right: 10px;
+		display: inline-block;
+	}
+	.info img{
+		width: 18px;
+		height: 18px;
+		vertical-align: middle;
+		padding-right: 10px;
+		display: inline-block;
 	}
 	#qz{
 		width: 30%;
